006c420a825f3b0924daca040794bf533857925e
Oscar <oscar.martinez.b@cua.uam.mx>
2025-04-22 21:22:00 -0600

    Creacion_de_proytecto

---
+++ src/main/java/mx/uam/chat/Chat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.uam.chat;
+
+/**
+ *
+ * @author marti
+ */
+public class Chat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
